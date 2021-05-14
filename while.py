058d1876ae67cb568f5464e35576c5ef13384f35
Helder Santos <heldersantos@bemol.onmicrosoft.com>
2021-05-14 13:28:04 -0400

    usando o while
    Signed-off-by: Helder Santos <heldersantos@bemol.onmicrosoft.com>

---
+++ while.py
@@ -0,0 +1,20 @@
+#
+
+numero = 5
+
+while numero <= 10:
+    print(numero)
+    numero = numero + 1
+
+    if numero == 8:
+        print("break")
+        break
+
+
+
+while numero <= 10:
+    print(numero)
+    numero = numero + 1
+
+else:
+    print("maior igual a 10")
