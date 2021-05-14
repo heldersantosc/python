69793590309c382ecaa72a04326a52dbda944f07
Helder Santos <heldersantos@bemol.onmicrosoft.com>
2021-05-14 13:23:32 -0400

    usando o for
    Signed-off-by: Helder Santos <heldersantos@bemol.onmicrosoft.com>

--- condicoes.py
+++ condicoes.py
@@ -8,3 +8,14 @@ elif total < 50:
     print("Menor que 50")
 else:
     print("Não é maior que 100 e nem menor que 50")
+
+
+nome_completo = "Helder Santos"
+
+for nome in nome_completo:
+    if not nome == "a":
+        print("Não é igual")
+
+for nome in nome_completo.split(" "):
+    if not nome == "Helder":
+        print(nome, "\nNão é igual a Helder")
---
+++ for.py
@@ -0,0 +1,9 @@
+#
+
+lista = [2, 4, 6, 8, 10, "Helder"]
+
+for item in lista:
+    print(item)
+
+for item in lista[0:2]:
+    print(item)
