e4ef3ab5b3a4868d9382b25b1a28519945692136
Helder Santos <heldersantos@bemol.onmicrosoft.com>
2021-05-14 13:07:42 -0400

    condicoes
    Signed-off-by: Helder Santos <heldersantos@bemol.onmicrosoft.com>

--- condicoes.py
+++ condicoes.py
@@ -1,1 +1,10 @@
-#
+# exemplicando as condições com if e else
+
+total = 100
+
+if total > 100:
+    print("Maior que 100")
+elif total < 50:
+    print("Menor que 50")
+else:
+    print("Não é maior que 100 e nem menor que 50")
